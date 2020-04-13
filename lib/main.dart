6b873a47bcdcbdd73490591b38ad2bab6dc81e4d
nickosmark <nickosmark@gmail.com>
2020-04-14 00:18:22 +0300

    changed color

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.red,
+        primarySwatch: Colors.blueGrey,
         fontFamily: 'dolphins',
       ),
       home: MyHomePage(title: 'This is a vcode2 test'),

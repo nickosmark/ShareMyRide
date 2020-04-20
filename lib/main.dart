107a1b89294a5d260960f6cd482cab3be7de12cb
chrissirk <48354894+chrissirk@users.noreply.github.com>
2020-04-20 21:43:52 +0300

    First commit (main)

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'screens/ProfileScreen.dart';
 import 'screens/ProfileEditScreen.dart';
-<<<<<<< HEAD
 import 'screens/LoginScreen.dart';
-=======
-import 'screens/RidesScreen.dart';
->>>>>>> messed up with classes
+import 'screens/ReviewsScreen.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,13 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-<<<<<<< HEAD
-      home: LoginScreen()
-=======
-      home: RidesScreen(),
->>>>>>> messed up with classes
+      home: ReviewsScreen()
     );
   }
 }
-
-

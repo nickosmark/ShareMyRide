2fc32b2163978c025f7336ea8aae6bba28b9459b
chrissirk <48354894+chrissirk@users.noreply.github.com>
2020-04-20 21:24:05 +0300

    Added revies screen

---
+++ lib/screens/ReviewsScreen.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/models/reviewModel.dart';
+import 'package:flutter_app/screens/ProfileScreen.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+
+class ReviewsScreen extends StatelessWidget {
+  final Color darkBlueColor = Color.fromRGBO(26, 26, 48, 1.0);
+  final Color lightBlueColor = Colors.blue;
+  final Color lightGreyBackground = Color.fromRGBO(229, 229, 229, 1.0);
+  final String name = "Ketoyla Perry";
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ShareMyRide',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        primaryColor: darkBlueColor,
+        accentColor: lightBlueColor,
+        textTheme: TextTheme(
+          body1: TextStyle(color: Color.fromRGBO(26, 26, 48, 1.0)),
+        ),
+      ),
+      home: Scaffold(
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              title: Text('HOME'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.drive_eta),
+              title: Text('RIDES'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle),
+              title: Text('PROFILE'),
+            ),
+          ],
+        ),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.only(top: 10.0),
+                  child: CircleAvatar(
+                      radius: 60.0,
+                      backgroundImage:
+                          new NetworkImage('https://via.placeholder.com/150')
+                  ),
+                ),
+                Text(
+                  name,
+                  style:
+                      GoogleFonts.oswald(textStyle: TextStyle(fontSize: 30.0)),
+                ),
+                RatingBar(
+                  initialRating: 3,
+                  minRating: 1,
+                  direction: Axis.horizontal,
+                  allowHalfRating: true,
+                  itemCount: 5,
+                  itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+                  itemBuilder: (context, _) => Icon(
+                    Icons.star,
+                    color: Colors.amber,
+                  ),
+                  onRatingUpdate: (rating) {
+                    print(rating);
+                  },
+                ),
+                Container(
+                  padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 10.0),
+                  child: TextField(
+                    obscureText: false,
+                    //expands: true,
+                    maxLines: 5,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0)
+                        ),
+                        labelText: "Enter your review here"
+                      ),
+                      textAlignVertical: TextAlignVertical.top,
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(top: 50.0, bottom: 100.0),
+                  child: RaisedButton(
+                    onPressed: () {},
+                    child: Text(
+                      "SUBMIT",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0)),
+                    color: darkBlueColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

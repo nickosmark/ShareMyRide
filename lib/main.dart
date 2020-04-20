219f59f7d8d11810d319b29bb4de3e7d6134b6d1
nickosmark <nickosmark@gmail.com>
2020-04-20 20:14:49 +0300

    random allagoula giati etsi

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'screens/ProfileScreen.dart';
 import 'screens/ProfileEditScreen.dart';
-<<<<<<< HEAD
 import 'screens/LoginScreen.dart';
-=======
 import 'screens/RidesScreen.dart';
->>>>>>> messed up with classes
 
 void main() => runApp(MyApp());
 
@@ -14,11 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-<<<<<<< HEAD
-      home: LoginScreen()
-=======
-      home: RidesScreen(),
->>>>>>> messed up with classes
+      home: RidesScreen()
     );
   }
 }

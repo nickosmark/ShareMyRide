b9ec9824e5ba39db686372cf8c828dda9d360f51
nickosmark <nickosmark@gmail.com>
2020-04-20 21:52:29 +0300

    ridesscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/screens/ConfirmScreen.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'screens/ProfileScreen.dart';
 import 'screens/ProfileEditScreen.dart';
--- lib/screens/RidesScreen.dart
+++ lib/screens/RidesScreen.dart
@@ -4,11 +4,13 @@ import 'package:flutter_app/widgets/reviewcard.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_app/models/reviewModel.dart';
 
+
+
 class RidesScreen extends StatelessWidget {
   var darkBlueColor = Color.fromRGBO(26, 26, 48, 1.0);
   var lightBlueColor = Colors.blue;
   var lightGreyBackground = Color.fromRGBO(229, 229, 229, 1.0);
-  //var review_1 = ReviewModel('https://randomuser.me/api/portraits/women/95.jpg','jinaa','shes the best', 5.0); 
+  //var review_1 = ReviewModel('https://randomuser.me/api/portraits/women/95.jpg','jinaa','shes the best', 5.0);
 
   @override
   Widget build(BuildContext context) {
@@ -44,128 +46,25 @@ class RidesScreen extends StatelessWidget {
           child: SingleChildScrollView(
             child: Column(
               children: <Widget>[
-                Container(
-                  margin:
-                      EdgeInsets.symmetric(vertical: 25.0, horizontal: 10.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                      IconButton(
-                          icon: Icon(Icons.edit),
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => ProfileScreen()),
-                            );
-                          }),
-                    ],
-                  ),
-                ),
-                CircleAvatar(
-                    radius: 60.0,
-                    backgroundImage:
-                        new NetworkImage('https://via.placeholder.com/150')),
-                Text(
-                  'Edit name',
-                  style:
-                      GoogleFonts.oswald(textStyle: TextStyle(fontSize: 30.0)),
-                ),
                 Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
                     Container(
-                      padding: EdgeInsets.only(right: 5.0),
+                      padding: EdgeInsets.symmetric(horizontal: 20.0,vertical:20.0),
                       child: Text(
-                        'My Rating:',
+                        'Pending',
                         style: GoogleFonts.oswald(
-                            textStyle: TextStyle(
-                          fontSize: 15.0,
-                        )),
+                            textStyle: TextStyle(fontSize: 30.0)),
                       ),
                     ),
-                    Container(
-                      padding: EdgeInsets.only(left: 5.0),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Text(
-                            '4,3',
-                            style: GoogleFonts.oswald(
-                                textStyle: TextStyle(
-                              fontSize: 15.0,
-                            )),
+                    IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.expand_more,
+                            size: 25.0,
+                            color: Colors.red,
                           ),
-                          Icon(
-                            Icons.star,
-                            size: 15.0,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-                Row(
-                  children: <Widget>[
-                    Container(
-                      padding: EdgeInsets.fromLTRB(15.0, 35.0, 0.0, 0.0),
-                      child: Text(
-                        'Personal Info',
-                        style: GoogleFonts.oswald(
-                            textStyle: TextStyle(
-                          fontSize: 20.0,
-                        )),
-                      ),
-                    ),
-                  ],
-                ),
-                Card(
-                  margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 15.0),
-                  child: ListTile(
-                    leading: Icon(Icons.phone),
-                    title: Text('+30 6999999999'),
-                  ),
-                ),
-                Card(
-                  margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 15.0),
-                  child: ListTile(
-                    leading: Icon(Icons.email),
-                    title: Text('covid@coronavirius.com'),
-                  ),
-                ),
-                Row(
-                  children: <Widget>[
-                    Container(
-                      padding: EdgeInsets.fromLTRB(15.0, 25.0, 0.0, 0.0),
-                      child: Text(
-                        'Car Info',
-                        style: GoogleFonts.oswald(
-                            textStyle: TextStyle(
-                          fontSize: 20.0,
-                        )),
-                      ),
-                    ),
-                  ],
-                ),
-                Card(
-                  margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 15.0),
-                  child: ListTile(
-                    leading: Icon(Icons.directions_car),
-                    title: Text('Mercendez Benz'),
-                  ),
-                ),
-                Row(
-                  children: <Widget>[
-                    Container(
-                      padding: EdgeInsets.fromLTRB(15.0, 25.0, 0.0, 0.0),
-                      child: Text(
-                        'Reviews',
-                        style: GoogleFonts.oswald(
-                            textStyle: TextStyle(
-                          fontSize: 20.0,
-                        )),
-                      ),
-                    ),
+                        ),
                   ],
                 ),
                 Card(
@@ -173,24 +72,119 @@ class RidesScreen extends StatelessWidget {
                   child: ListTile(
                     leading: CircleAvatar(
                         backgroundImage: new NetworkImage(
-                            'https://images.unsplash.com/photo-1510227272981-87123e259b17?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=3759e09a5b9fbe53088b23c615b6312e')),
-                    title: Text('Jina Jinoglou'),
-                    subtitle: Text(
-                        'Amazing driver!! She helped me with my anxiety. The best part is that she is also a Trump supporter :)'),
-                    isThreeLine: true,
+                            'https://randomuser.me/api/portraits/women/95.jpg')),
+                    title: Text('Julia Alexandratou'),
+                    subtitle: Text('Athens -> Patras'),
                     trailing: Row(
                       mainAxisSize: MainAxisSize.min,
                       children: <Widget>[
-                        Text('5,0'),
-                        Icon(
-                          Icons.star,
-                          size: 15.0,
+                        Padding(
+                          padding: const EdgeInsets.only(right: 30.0),
+                          child: IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.check,
+                              size: 25.0,
+                              color: Colors.green,
+                            ),
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.close,
+                            size: 25.0,
+                            color: Colors.red,
+                          ),
                         ),
                       ],
                     ),
                   ),
                 ),
-                //reviewCard(review_1.getUrl(),review_1.getName(),'sheee is the best', 5.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 20.0,vertical:20.0),
+                      child: Text(
+                        'Confirmed',
+                        style: GoogleFonts.oswald(
+                            textStyle: TextStyle(fontSize: 30.0)),
+                      ),
+                    ),
+                    IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.expand_more,
+                            size: 25.0,
+                            color: Colors.red,
+                          ),
+                        ),
+                  ],
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 15.0),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                        backgroundImage: new NetworkImage(
+                            'https://randomuser.me/api/portraits/women/95.jpg')),
+                    title: Text('Julia Alexandratou'),
+                    subtitle: Text('Athens -> Patras'),
+                    trailing: Padding(
+                      padding: const EdgeInsets.only(right: 0.0),
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.cancel,
+                          size: 25.0,
+                          color: Colors.redAccent,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 20.0,vertical:20.0),
+                      child: Text(
+                        'Completed',
+                        style: GoogleFonts.oswald(
+                            textStyle: TextStyle(fontSize: 30.0)),
+                      ),
+                    ),
+                    IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.expand_more,
+                            size: 25.0,
+                            color: Colors.red,
+                          ),
+                        ),
+                  ],
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 15.0),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                        backgroundImage: new NetworkImage(
+                            'https://randomuser.me/api/portraits/women/95.jpg')),
+                    title: Text('Julia Alexandratou'),
+                    subtitle: Text('Athens -> Patras'),
+                    trailing: Padding(
+                      padding: const EdgeInsets.only(right: 0.0),
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.chat,
+                          size: 25.0,
+                          color: darkBlueColor,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
           ),

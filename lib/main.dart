2fadf2bb0362e7926d80c1acd3888dd4116d0ae0
chrissirk <48354894+chrissirk@users.noreply.github.com>
2020-04-28 22:34:05 +0300

    Fake changes for less merges

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/models/UserModel.dart';
-//import 'package:flutter_app/screens/HomeScreen.dart';
+import 'package:flutter_app/screens/HomeScreen.dart';
 import 'screens/ProfileScreen.dart';
 import 'screens/RidesScreen.dart';
 import 'services/fakeDB.dart';
-import 'package:flutter_app/screens/chrisHomeScreen.dart';
-//import 'package:flutter_app/screens/HomeScreen.dart';
+import 'package:flutter_app/screens/MyApp.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MyApp(selectedIndex: 0,));
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
 
-class _MyAppState extends State<MyApp> {
-  //Created a random User from fake DB.
-  UserModel randomUser12 = FakeDB.randomUser12;
-
-  //Selected Icon in the bottomNavBar
-  int _selectedIndex = 0;
-
-  //The screen showing first
-  Widget _selectedScreen = ChrisHomeScreen();
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        bottomNavigationBar: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              title: Text('HOME'),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.drive_eta),
-              title: Text('RIDES'),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.account_circle),
-              title: Text('PROFILE'),
-            ),
-          ],
-          currentIndex: _selectedIndex,
-          selectedItemColor: Colors.blue,
-          onTap: (index) {
-            setState(() {
-              _selectedIndex = index;
-              switch (_selectedIndex) {
-                case 0:
-                  _selectedScreen = ChrisHomeScreen();
-                  break;
-                case 1:
-                  _selectedScreen = RidesScreen();
-                  break;
-                case 2:
-                  _selectedScreen = ProfileScreen(userModel: randomUser12);
-                  break;
-              }
-            });
-          },
-        ),
-        body: _selectedScreen,
-      ),
-    );
-  }
-}
 
 // class MyApp extends StatelessWidget {
 //   @override

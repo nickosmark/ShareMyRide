b1fff6a94afbf8c4fd3fa704c0045c423049e3d0
chrissirk <48354894+chrissirk@users.noreply.github.com>
2020-04-28 18:39:30 +0300

    Added stuff on showDetails()

--- lib/screens/chrisHomeScreen.dart
+++ lib/screens/chrisHomeScreen.dart
@@ -6,6 +6,8 @@ import 'package:flutter/services.dart' show rootBundle;
 import 'package:intl/intl.dart';
 import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
 import 'package:flutter_app/widgets/RideResultCard.dart';
+import 'package:flutter_app/widgets/ReviewCard.dart';
+import 'package:flutter_app/models/ReviewModel.dart';
 
 class ChrisHomeScreen extends StatefulWidget {
   @override
@@ -25,6 +27,11 @@ class _HomeScreenState extends State<ChrisHomeScreen> {
   String date;
   List<Widget> results;
   RideResultCard resultCard = new RideResultCard();
+  static ReviewModel reviewModel = new ReviewModel(imageUrl: 'https://img.documentonews.gr/unsafe/1000x600/smart/http://img.dash.documentonews.gr/documento/imagegrid/2018/10/31/5bd98303cd3a18740d2cf935.jpg',
+      name:'Your dad/mom', reviewText:'The worst driver... Women drive better than non-binarys!', rating: 0.5);
+  static ReviewCard reviewCard = new ReviewCard(reviewModel: reviewModel);
+  List <ReviewCard> reviewList = [reviewCard, reviewCard, reviewCard, reviewCard, reviewCard, reviewCard];
+  bool showResults = true;
 
   @override
   void dispose() {
@@ -98,7 +105,7 @@ class _HomeScreenState extends State<ChrisHomeScreen> {
                 date = dateController.text;
                 setState(() {
                   mode = 1;
-                  results = [_listItem(), _listItem(), _listItem()];
+                  results = [_listResultItem(), _listResultItem(), _listResultItem()];
                 });
               }, //onPressed
               label: Text('Search'),
@@ -140,8 +147,7 @@ class _HomeScreenState extends State<ChrisHomeScreen> {
     );
   }
 
-  Align fillField(
-      String str, Color clr, double top, TextEditingController controller) {
+  Align fillField(String str, Color clr, double top, TextEditingController controller) {
     return Align(
         alignment: Alignment.topCenter,
         child: Padding(
@@ -199,7 +205,7 @@ class _HomeScreenState extends State<ChrisHomeScreen> {
   Scaffold searchScaffold() {
     return Scaffold(
       body: Stack(
-        children: <Widget>[_googleMap(context), _showResults()],
+        children: <Widget>[_googleMap(context), _showResults(), _showDetails()],
       ),
     );
   }
@@ -207,78 +213,239 @@ class _HomeScreenState extends State<ChrisHomeScreen> {
   Widget _showResults() {
     return Align(
         alignment: Alignment.bottomLeft,
-        child: SizedBox(
-          height: 230.0,
-          child: DecoratedBox(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(topLeft: Radius.circular(20.0), topRight: Radius.circular(20.0)),
-              color: Colors.white,
-            ),
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 10),
-                  child: Align(
-                    alignment: Alignment.topLeft,
-                    child: Text(
-                      'Hey Xzhibit!',
-                      style: TextStyle(
-                        fontSize: 22.0
+        child: Visibility(
+          visible: showResults,
+          child: SizedBox(
+            height: 230.0,
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40.0),
+                    topRight: Radius.circular(40.0)),
+                color: Colors.white,
+              ),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 25),
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        'Hey Xzhibit!',
+                        style: TextStyle(fontSize: 22.0),
                       ),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 10),
-                  child: Align(
-                    alignment: Alignment.topLeft,
-                    child: Text(
-                      'We have found these results for you.',
-                      style: TextStyle(
-                          fontSize: 15.0
+                  Padding(
+                    padding: const EdgeInsets.only(left: 25),
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        'We have found these results for you.',
+                        style: TextStyle(fontSize: 15.0),
                       ),
                     ),
                   ),
-                ),
-                Expanded(
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: <Widget>[
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      _listItem(),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      _listItem(),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      _listItem(),
-                    ],
+                  Expanded(
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: <Widget>[
+                        SizedBox(
+                          width: 10.0,
+                        ),
+                        _listResultItem(),
+                        SizedBox(
+                          width: 10.0,
+                        ),
+                        _listResultItem(),
+                        SizedBox(
+                          width: 10.0,
+                        ),
+                        _listResultItem(),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ));
   } //buildContainer
 
-  Widget _listItem() {
+  Widget _listResultItem() {
     resultCard = new RideResultCard(from: from, to: to);
     return GestureDetector(
       onTap: () {
         setState(() {
-          mode = 0;
+          showResults = false;
         });
       },
-      child: Container(
-        width: 300,
-        child: resultCard
+      child: Container(width: 300, child: resultCard),
+    );
+  } //list items
+
+  Widget _showDetails() {
+
+    return Align(
+      alignment: Alignment.bottomLeft,
+      child: Visibility(
+        visible: !showResults,
+        child: GestureDetector(
+
+          child: Stack(
+            alignment: AlignmentDirectional.bottomCenter,
+            children: <Widget>[
+              SizedBox(
+                //first white box
+                height: 500.0,
+                width: double.infinity,
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(40.0),
+                        topRight: Radius.circular(40.0)),
+                    color: Colors.white,
+                  ),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: new NetworkImage(
+                          'https://img.documentonews.gr/unsafe/1000x600/smart/http://img.dash.documentonews.gr/documento/imagegrid/2018/10/31/5bd98303cd3a18740d2cf935.jpg'),
+                      radius: 30.0,
+                    ),
+                    title: Text(
+                      'Jason Antigoniiiiiiiiiiii',
+                      style: TextStyle(fontSize: 20.0),
+                    ),
+                    subtitle: Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.directions_car,
+                          size: 20.0,
+                        ),
+                        SizedBox(
+                          width: 10.0,
+                        ),
+                        Text(
+                          'Batmobile',
+                          style: TextStyle(fontSize: 20.0),
+                        ),
+                      ],
+                    ),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Text('1.0'),
+                        Icon(
+                          Icons.star,
+                          size: 15.0,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                //second blue box
+                height: 400.0,
+                width: double.infinity,
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(40.0),
+                        topRight: Radius.circular(40.0)),
+                    color: Colors.deepPurple[900],
+                  ),
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.only(top: 15.0, left: 5.0, right: 5.0),
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                          flex: 1,
+                          child: Column(
+                            children: <Widget>[
+                              Text(
+                                from,
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    fontSize: 20.0, color: Colors.white),
+                              ),
+                              Icon(
+                                Icons.arrow_downward,
+                                size: 30.0,
+                                color: Colors.white,
+                              ),
+                              Text(
+                                to,
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    fontSize: 20.0, color: Colors.white),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: Column(
+                            children: <Widget>[
+                              SizedBox(
+                                height: 10.0,
+                              ),
+                              Text(
+                                'Cost Estimation:',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    fontSize: 20.0, color: Colors.white),
+                              ),
+                              SizedBox(
+                                height: 30.0,
+                              ),
+                              Text(
+                                '200.22€',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    fontSize: 30.0, color: Colors.white),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                //third white box
+                height: 250.0,
+                width: double.infinity,
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(40.0),
+                        topRight: Radius.circular(40.0)),
+                    color: Colors.white,
+                  ),
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Expanded(
+                        child: ListView(
+                          children: reviewList,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
-  }//boxes
+  } //showDetails
+
 } //build_end
 
 Marker firstmarker = Marker(

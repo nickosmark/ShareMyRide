b219f7b87cf20a394fdd08c801d78268a11cfa2f
chrissirk <48354894+chrissirk@users.noreply.github.com>
2020-05-02 19:25:40 +0300

    Unfinished changes on searchEngine

--- lib/services/SearchEngine.dart
+++ lib/services/SearchEngine.dart
@@ -1,14 +1,31 @@
 import 'package:flutter_app/models/ReviewModel.dart';
 import 'package:flutter_app/models/RidesModel.dart';
 import 'package:flutter_app/models/UserModel.dart';
+import 'package:flutter_app/widgets/RideResultCard.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class SearchEngine{
 
-  String searchFrom;
-  String searchTo;
+  String fromText;
+  String toText;
   LatLng fromCords;
   LatLng toCords;
   DateTime searchDate;
+
+  SearchEngine(String fromText, String toText, LatLng fromCords, toCords, DateTime date) {
+    this.fromText = fromText;
+    this.toText = toText;
+    this.fromCords = fromCords;
+    this.toCords = toCords;
+    this.searchDate = date;
+  }
+
+  List<RideResultCard> getResults() {
+    List<RideResultCard> results;
+
+
+
+    return results;
+  }
 
 }

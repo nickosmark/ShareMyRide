e31ddb11c260e0dce610433ad884517544f4b99d
nickosmark <nickosmark@gmail.com>
2020-05-22 16:43:29 +0300

    fixed back button

--- lib/screens/MyApp.dart
+++ lib/screens/MyApp.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_app/models/UserModel.dart';
 import 'package:flutter_app/screens/HomeScreen.dart';
 import 'package:flutter_app/screens/ProfileScreen.dart';
@@ -85,51 +86,56 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        fontFamily: 'fira',
-        textTheme: TextTheme(
-          body1: TextStyle(
-            fontFamily: 'fira',
+    return WillPopScope(
+      onWillPop: (){
+        return SystemNavigator.pop();
+      },
+      child: MaterialApp(
+        theme: ThemeData(
+          fontFamily: 'fira',
+          textTheme: TextTheme(
+            body1: TextStyle(
+              fontFamily: 'fira',
+            ),
           ),
         ),
-      ),
-      home: Scaffold(
-        bottomNavigationBar: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              title: Text('HOME'),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.drive_eta),
-              title: Text('RIDES'),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.account_circle),
-              title: Text('PROFILE'),
-            ),
-          ],
-          currentIndex: _selectedIndex,
-          selectedItemColor: Colors.blue,
-          onTap: (index) {
-            setState(() {
-              _selectedIndex = index;
-              switch (_selectedIndex) {
-                case 0:
-                  _selectedScreen = ChrisHomeScreen(db: db,);
-                  break;
-                case 1:
-                  _selectedScreen = RidesScreen(db: db);
-                  break;
-                case 2:
-                  _selectedScreen = ProfileScreen(db: db,);
-                  break;
-              }
-            });
-          },
+        home: Scaffold(
+          bottomNavigationBar: BottomNavigationBar(
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                title: Text('HOME'),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.drive_eta),
+                title: Text('RIDES'),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.account_circle),
+                title: Text('PROFILE'),
+              ),
+            ],
+            currentIndex: _selectedIndex,
+            selectedItemColor: Colors.blue,
+            onTap: (index) {
+              setState(() {
+                _selectedIndex = index;
+                switch (_selectedIndex) {
+                  case 0:
+                    _selectedScreen = ChrisHomeScreen(db: db,);
+                    break;
+                  case 1:
+                    _selectedScreen = RidesScreen(db: db);
+                    break;
+                  case 2:
+                    _selectedScreen = ProfileScreen(db: db,);
+                    break;
+                }
+              });
+            },
+          ),
+          body: _selectedScreen,
         ),
-        body: _selectedScreen,
       ),
     );
   }
